d18335d352403254e87b4c8b26ac5ede23fa3a71
Muhamad Syauqi Jamil <syauqi.j@outlook.com>
2020-11-20 17:42:33 +0800

    Styling base template layout.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,33 @@
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body class="bg-gray-100">
+    <nav class="p-6 bg-white flex justify-between mb-6">
+        <ul class="flex item-center">
+            <li>
+                <a href="" class="p-3"> Home </a>
+            </li>
+            <li>
+                <a href="" class="p-3"> Dashboard </a>
+            </li>
+            <li>
+                <a href="" class="p-3"> Form </a>
+            </li>
+        </ul>
+        <ul class="flex item-center">
+            <li>
+                <a href="" class="p-3"> Syauqi Jamil </a>
+            </li>
+            <li>
+                <a href="" class="p-3"> Login </a>
+            </li>
+            <li>
+                <a href="" class="p-3"> Register </a>
+            </li>
+            <li>
+                <a href="" class="p-3"> Logout </a>
+            </li>
+        </ul>
+    </nav>
     @yield('content')
 </body>
 </html>
